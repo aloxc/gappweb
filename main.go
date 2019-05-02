e757788f3b98381a218f9571d5f3eacd5bd96bd7
aloxc <leerohwa@gmail.com>
2019-05-03 00:20:31 +0800

    完成2个模块功能，准备放到docker环境中去

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"github.com/aloxc/gappweb/config"
 	_ "github.com/aloxc/gappweb/router"
 	"github.com/astaxie/beego"
+	"os"
 )
 
 func main() {
 	beego.BConfig.WebConfig.Session.SessionOn = true
-	beego.Run()
+	port := os.Getenv(config.WEB_SERVER_PORT)
+	if port == "" {
+		beego.Run()
+	} else {
+		beego.Run(port)
+	}
 }
